7bd6a6c04b29ab28bdc2b6b0fd28c44c91921920
Michelle-Hmzk <michellehmzk@gmail.com>
2022-03-24 11:00:26 -0300

    Criação do arvivo sistema

--- AULA-06/MauaBank/src/Titulo.java
+++ AULA-06/MauaBank/src/Titulo.java
@@ -1,39 +1,28 @@
 import java.time.LocalDate;
 
 public class Titulo {
-    //conta que precisa ser paga = valor, data de vencimento e multa
+    // conta que precisa ser paga = valor, data de vencimento e multa
     private double valor;
     private LocalDate data;
     private double multaDiaria;
 
-    //Precisamos de uma data
-    public Titulo (double valor, LocalDate data , double multaDiaria){
-        this.setValor(valor) ;
-        this.setData(data); 
-        this.setMultaDiaria(multaDiaria);
-}
+    // Precisamos de uma data
+    public Titulo(double valor, LocalDate data, double multaDiaria) {
+        this.valor = valor;
+        this.data = data;
+        this.multaDiaria = multaDiaria;
+    }
 
     public double getMultaDiaria() {
         return multaDiaria;
-    }
-
-    public void setMultaDiaria(double multaDiaria) {
-        this.multaDiaria = multaDiaria;
     }
 
     public LocalDate getData() {
         return data;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
     public double getValor() {
         return valor;
     }
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-    }
-        
+
+}
